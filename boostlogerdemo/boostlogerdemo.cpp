ef0efae97a1a416efb010d66c22a0ae999c19daa
DevMayurJ <jadhav.mayur30@gmail.com>
2020-04-30 17:41:43 +0530

    Removed commented code

--- boostlogerdemo/boostlogerdemo.cpp
+++ boostlogerdemo/boostlogerdemo.cpp
@@ -1,36 +1,3 @@
-/*#include <iostream>
-
-#include <boost/log/trivial.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/expressions.hpp>
-
-namespace logging = boost::log;
-
-void init_logging()
-{
-	logging::add_file_log("sample.log");
-
-	logging::core::get()->set_filter
-	(
-		logging::trivial::severity >= logging::trivial::info
-	);
-}
-
-int main(int, char*[])
-{
-	init_logging();
-
-	BOOST_LOG_TRIVIAL(trace) << "This is a trace severity message";
-	BOOST_LOG_TRIVIAL(debug) << "This is a debug severity message";
-	BOOST_LOG_TRIVIAL(info) << "This is an informational severity message";
-	BOOST_LOG_TRIVIAL(warning) << "This is a warning severity message";
-	BOOST_LOG_TRIVIAL(error) << "This is an error severity message";
-	BOOST_LOG_TRIVIAL(fatal) << "and this is a fatal severity message";
-	std::cin.get();
-	return 0;
-}*/
-
-
 #include <iostream>
 
 #include <boost/log/core.hpp>

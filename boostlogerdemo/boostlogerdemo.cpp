c793ec10fa7bc4ce5246d06f0f6b3baa59d7ee69
DevMayurJ <jadhav.mayur30@gmail.com>
2020-04-30 17:39:12 +0530

    Added first version of logger using boost library
    1.Download boost lib from https://www.boost.org/users/download/
    2.To generate boost libs:
    If you wish to build from source with Visual C++, you can use a simple build procedure described in this section. Open the command prompt and change your current directory to the Boost root directory. Then, type the following commands:
    
    bootstrap
    .\b2
    The first command prepares the Boost.Build system for use. The second command invokes Boost.Build to build the separately-compiled Boost libraries. Please consult the Boost.Build documentation for a list of allowed options.

---
+++ boostlogerdemo/boostlogerdemo.cpp
@@ -0,0 +1,78 @@
+/*#include <iostream>
+
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/expressions.hpp>
+
+namespace logging = boost::log;
+
+void init_logging()
+{
+	logging::add_file_log("sample.log");
+
+	logging::core::get()->set_filter
+	(
+		logging::trivial::severity >= logging::trivial::info
+	);
+}
+
+int main(int, char*[])
+{
+	init_logging();
+
+	BOOST_LOG_TRIVIAL(trace) << "This is a trace severity message";
+	BOOST_LOG_TRIVIAL(debug) << "This is a debug severity message";
+	BOOST_LOG_TRIVIAL(info) << "This is an informational severity message";
+	BOOST_LOG_TRIVIAL(warning) << "This is a warning severity message";
+	BOOST_LOG_TRIVIAL(error) << "This is an error severity message";
+	BOOST_LOG_TRIVIAL(fatal) << "and this is a fatal severity message";
+	std::cin.get();
+	return 0;
+}*/
+
+
+#include <iostream>
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+
+void init_logging()
+{
+	logging::register_simple_formatter_factory<logging::trivial::severity_level, char>("Severity");
+
+	logging::add_file_log
+	(
+		keywords::file_name = "sample.log",
+		keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+	);
+
+	logging::core::get()->set_filter
+	(
+		logging::trivial::severity >= logging::trivial::trace
+	);
+
+	logging::add_common_attributes();
+}
+
+int main(int, char*[])
+{
+	init_logging();
+
+	int i = 10;
+	BOOST_LOG_TRIVIAL(trace) << "TESTTTTTTTTT This is a trace severity message" << i;
+	BOOST_LOG_TRIVIAL(debug) << "This is a debug severity message";
+	BOOST_LOG_TRIVIAL(info) << "This is an informational severity message";
+	BOOST_LOG_TRIVIAL(warning) << "This is a warning severity message";
+	BOOST_LOG_TRIVIAL(error) << "This is an error severity message";
+	BOOST_LOG_TRIVIAL(fatal) << "and this is a fatal severity message";
+
+	std::cout << "Enter any key to exit: ";
+	std::cin.get();
+	return 0;
+}
